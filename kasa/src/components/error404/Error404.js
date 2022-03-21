19dde486ce2fa330bd35f5c3cd06105bdb4684c2
Manel.Defnoune <36042435+manel009@users.noreply.github.com>
2022-03-21 13:49:07 +0100

    Error 404

--- kasa/src/components/error404/Error404.js
+++ kasa/src/components/error404/Error404.js
@@ -1,7 +1,15 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+import './Error404.css'; 
 
 export default function Error404(){
     return (
-        <h1>404</h1>
+        <>
+        <section className="error404">
+            <h1>404</h1>
+            <h3> Oups! La page que vous demandez n'existe pas. </h3>
+            <Link to="/"> Retourner sur la page d'accueil</Link>
+        </section>
+        </>
     );
 }
